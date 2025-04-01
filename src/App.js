1cbdd7663658dfddcabd7872d9227e3e6370b03e
monjurulhaquerajun <monjurulhaquerajun@gmail.com>
2025-04-01 20:35:56 +0600

    declare and using component

--- src/App.js
+++ src/App.js
@@ -2,41 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const person = {
-    name: "Salam Uddin",
-    age: 32
-  }
-  const person2 = {
-    name : "Kalam Uddin",
-    age: 47
-  }
-  var style = {
-    color: 'blue',
-    backgroundColor: 'yellow'
-    
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>this is paragraph</p>
-        <h2 style={style}>First person : {person.name}, {person.age}</h2>
-        <h2 style={{color:'red', backgroundColor: 'tomato'}}>Second person : {person2.name}, {person2.age}</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>This is a paragraph</p>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
- 
+function Person() {
+  return (
+    <div>
+      <h1>Name : Abdul Ali</h1>
+      <h1>Age : 35</h1>
+    </div>
+
+  )
+}
+
 export default App;
