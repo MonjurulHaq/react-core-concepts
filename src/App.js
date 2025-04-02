3e901cfaf8d6b6fa27467d2b3db7c6d1fc14fb33
monjurulhaquerajun <monjurulhaquerajun@gmail.com>
2025-04-02 09:27:57 +0600

    event handlers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const names = ['Aziz','Rakib', 'Sakib', 'John', 'Ron']
   const products = [
     {name: "Photoshop", price: "56$"},
     {name: "ChatGPt", price: "40$"},
@@ -10,12 +12,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <ul>
+          {names.map(name => <li>{name}</li>)}
+        </ul>
+        <Counter></Counter>
         <Product product = {products[0]}></Product>
         <Product product = {products[1]}></Product>
         <Product product = {products[2]}></Product>
       </header>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(0);
+  const handelCount = () => setCount(count + 1)
+  return(
+    <div>
+    <h2>Count: {count} </h2>
+    <button onClick={handelCount}>Click me</button>
+  </div>
+  )
 }
 
 function Product(props){
